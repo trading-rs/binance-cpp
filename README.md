7ed9f5225a459a49a2fb8a46cedf85d0a4c31af3
zjhmale <zjhmale@gmail.com>
2017-10-18 19:10:11 +0800

    CI :rocket:

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.org/zjhmale/binance-cpp.svg?branch=master)](https://travis-ci.org/zjhmale/binance-cpp)
+
+## Build, Test and Run
+
+```
+./build.sh
+```
--- test/tests.cc
+++ test/tests.cc
@@ -1,6 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "../src/endpoint.hpp"
+using namespace endpoint;
+
+#include <memory>
+using namespace std;
+
+const char* api_key = getenv("BINANCE_API_KEY");
+const char* api_secret = getenv("BINANCE_API_SECRET");
+
+auto pre_check() {
+  return api_key && api_secret;
+}
+
 TEST_CASE("Binance API") {
-  REQUIRE(1 == 1);
+  REQUIRE(pre_check());
+  auto endpoint = make_shared<Endpoint>(api_key, api_secret);
+
+  REQUIRE(endpoint->ping() != nullptr);
+  REQUIRE(endpoint->time() != nullptr);
 }
