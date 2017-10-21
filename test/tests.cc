dbae9c5d38a42d448fe1c6c998233403e6b97acc
zjhmale <zjhmale@gmail.com>
2017-10-21 16:16:53 +0800

    Fixes #4

--- test/tests.cc
+++ test/tests.cc
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-#include "../src/endpoint.hpp"
+#include <binance/endpoint.hpp>
 using namespace endpoint;
 
 #include <memory>
