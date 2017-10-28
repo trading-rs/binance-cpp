ac8ca07bf9f08d1b95d2912989d10100d8ef859c
zjhmale <zjhmale@gmail.com>
2017-10-28 13:15:34 +0800

    Fixes #10

--- example/depth_cache_example.cc
+++ example/depth_cache_example.cc
@@ -14,29 +14,40 @@ namespace spd = spdlog;
 #include <algorithm>
 using namespace std;
 
+typedef map<double, double, greater<double>> SortedEntries;
+typedef map<string, SortedEntries> DepthCache;
+
 const char* api_key = getenv("BINANCE_API_KEY");
 const char* api_secret = getenv("BINANCE_API_SECRET");
+const string bid_key = "BIDS";
+const string ask_key = "ASKS";
+
+shared_ptr<spd::logger> cache_log = spd::stdout_color_mt("DEPTHCACHE_EXAMPLE");
+
+DepthCache depth_cache;
+long limit = 10;
+long last_update_id;
 
 auto pre_check() {
   if (!(api_key && api_secret))
     throw runtime_error("Please setup binance APIKEY and APISECRET!");
 }
 
-map<string, map<double, double, greater<double>>> depth_cache;
-long limit = 10;
-long last_update_id;
-const string bid_key = "BIDS";
-const string ask_key = "ASKS";
+auto get_sorted_asks() -> SortedEntries {
+  return depth_cache[ask_key];
+}
 
-shared_ptr<spd::logger> cache_log = spd::stdout_color_mt("DEPTHCACHE_EXAMPLE");
+auto get_sorted_bids() -> SortedEntries {
+  return depth_cache[bid_key];
+}
 
 auto initialize_depth_cache(const string &symbol, shared_ptr<Endpoint> client) -> void {
   Maybe<OrderBook> order_book = client->order_book(symbol, 10);
   if (order_book.isJust()) {
     OrderBook ob = order_book.fromJust();
     last_update_id = ob.last_update_id;
-    map<double, double, greater<double>> asks;
-    map<double, double, greater<double>> bids;
+    SortedEntries asks;
+    SortedEntries bids;
     std::for_each(ob.asks.cbegin(), ob.asks.cend(), [&](const OrderBookEntry &obe) {
         asks[stod(obe.price)] = stod(obe.qty);
       });
@@ -50,7 +61,7 @@ auto initialize_depth_cache(const string &symbol, shared_ptr<Endpoint> client) -
   }
 }
 
-auto update_depth_cache(map<double, double, greater<double>> caches, const vector<OrderBookEntry> deltas) -> void {
+auto update_depth_cache(SortedEntries caches, const vector<OrderBookEntry> deltas) -> void {
   std::for_each(deltas.cbegin(), deltas.cend(), [&](const OrderBookEntry &delta) {
       double price = stod(delta.price);
       double quantity = stod(delta.qty);
@@ -65,18 +76,31 @@ auto update_depth_cache(map<double, double, greater<double>> caches, const vecto
 auto print_depth_cache() -> void {
   cout << ">>>>>>DEPTHCACHE>>>>>>" << endl;
   cout << "ASKS:" << endl;
-  auto asks = depth_cache[ask_key];
+  auto asks = get_sorted_asks();
   std::for_each(asks.cbegin(), asks.cend(), [](const pair<double, double> &pair) {
       cout << format("{0} / {1}", pair.first, pair.second) << endl;
     });
   cout << "BIDS:" << endl;
-  auto bids = depth_cache[bid_key];
+  auto bids = get_sorted_bids();
   std::for_each(bids.cbegin(), bids.cend(), [](const pair<double, double> &pair) {
       cout << format("{0} / {1}", pair.first, pair.second) << endl;
     });
   cout << format("BEST ASK: {0} / {1}", asks.crbegin()->first, asks.crbegin()->second) << endl;
   cout << format("BEST BID: {0} / {1}", bids.cbegin()->first, bids.cbegin()->second) << endl;
   cout << "<<<<<<DEPTHCACHE<<<<<<" << endl;
+}
+
+auto print_depth_volume() -> void {
+  auto asks = get_sorted_asks();
+  auto bids = get_sorted_bids();
+  pair<double, double> ask_pair = accumulate(asks.cbegin(), asks.cend(), make_pair(0.0, 0.0), [](const pair<double, double> &result,                                       const pair<double, double> &pair) {
+      return make_pair(result.first + (pair.first * pair.second), result.second + pair.second);
+    });
+  pair<double, double> bid_pair = accumulate(bids.cbegin(), bids.cend(), make_pair(0.0, 0.0), [](const pair<double, double> &result,                                       const pair<double, double> &pair) {
+      return make_pair(result.first + (pair.first * pair.second), result.second + pair.second);
+    });
+  cout << format("ASK VOLUME: {0} / {1}", ask_pair.first, ask_pair.second) << endl;
+  cout << format("BID VOLUME: {0} / {1}", bid_pair.first, bid_pair.second) << endl;
 }
 
 auto start_depth_event_streaming(string symbol, shared_ptr<Endpoint> client) -> void {
@@ -86,9 +110,10 @@ auto start_depth_event_streaming(string symbol, shared_ptr<Endpoint> client) -> 
       if (de.update_id > last_update_id) {
         print_result<DepthEvent>(de);
         last_update_id = de.update_id;
-        update_depth_cache(depth_cache[ask_key], de.asks);
-        update_depth_cache(depth_cache[bid_key], de.bids);
+        update_depth_cache(get_sorted_asks(), de.asks);
+        update_depth_cache(get_sorted_bids(), de.bids);
         print_depth_cache();
+        print_depth_volume();
       }
     });
 }
